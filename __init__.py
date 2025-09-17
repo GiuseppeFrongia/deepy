1b4c72d18520cc6ea37f42afb1f330a51655d28f
GiuseppeFrongia <giuseppefrongia00@gmail.com>
2025-09-17 09:30:03 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,22 +1,20 @@
-from .models import Model, VAEModel
-from .loss_func import LossFunc, MeanSquaredError, BinaryCrossEntropy, CategoricalCrossEntropy, VAEBoW
+from .models import Model
+from .loss_func import LossFunc, MeanSquaredError, BinaryCrossEntropy, CategoricalCrossEntropy
 from .activations import Activation, Linear, ReLU, Sigmoid, Softmax
 from .optimizers import Optimizer, SGD
 from .utils import confusion_matrix
 from .layers.core import Layer, Input, Dense, Output
 
-__version__ = "2025.0.1"
+__version__ = "2025.1.0"
 
 
 __all__ = [
     "__version__",      #From models.py
     "Model",
-    "VAEModel",
     "LossFunc",         #From loss_func.py
     "MeanSquaredError",
     "BinaryCrossEntropy",
     "CategoricalCrossEntropy",
-    "VAEBoW",
     "Activation",       #From activations.py
     "Linear",
     "ReLU",
